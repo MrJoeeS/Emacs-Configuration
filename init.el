286a59f197f5ded12d8900848b2f59f115f9fd10
MrJoeeS <93727587+MrJoeeS@users.noreply.github.com>
2024-03-27 22:46:47 -0400

    Update init.el
    Cleaned up init.el and moved to use-packages with hooks

--- init.el
+++ init.el
@@ -2,7 +2,6 @@
 ;; 01 Emacs package management
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
@@ -16,13 +15,21 @@
    '("db86c52e18460fe10e750759b9077333f9414ed456dc94473f9cf188b197bc74" default))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(prettier-js lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode)))
+   '(dumb-jump prettier-js lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Ubuntu Condensed" :foundry "DAMA" :slant normal :weight regular :height 158 :width normal)))))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+(which-key-mode)
+(helm-mode)
 
 (use-package treesit-auto
   :custom
@@ -31,62 +38,45 @@
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
 
-(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode zenburn-theme json-mode))
-(when (cl-find-if-not #'package-installed-p package-selected-packages)
-  (package-refresh-contents)
-  (mapc #'package-install package-selected-packages))
+(use-package prettier-js
+  :ensure t
+  :hook (
+	  (js-ts-mode . prettier-js-mode)
+	  (js2-mode . prettier-js-mode)
+	  (web-mode . prettier-js-mode)
+	  )
+  )
 
-(helm-mode)
-(require 'helm-xref)
-(define-key global-map [remap find-file] #'helm-find-files)
-(define-key global-map [remap execute-extended-command] #'helm-M-x)
-(define-key global-map [remap switch-to-buffer] #'helm-mini)
-(which-key-mode)
-(add-hook 'prog-mode-hook #'lsp)
+(use-package python-black
+  :ensure t
+  :after python
+  :hook (python-ts-mode . python-black-on-save-mode))
+
+(use-package eglot
+  :ensure t
+  :hook (
+	 (prog-mode . eglot-ensure)
+	 (prog-mode . hs-minor-mode)
+	 (prog-mode . global-company-mode)
+	 (prog-mode . yas-global-mode)
+     	 )
+  )
+
+(setq python-indent-level 4)
+(setq js-indent-level 2)
+
+(use-package helm-xref
+  :ensure t
+  :bind (
+	 ([remap find-file] . helm-find-files)
+	 ([remap execute-extended-command] . helm-M-x)
+	 ([remap switch-to-buffer] . helm-mini)
+	 )
+  )
+
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024)
       company-idle-delay 0.0
       company-minimum-prefix-length 1
       create-lockfiles nil) ;; lock files will kill `npm start'
-(with-eval-after-load 'lsp-mode
-  (require 'dap-chrome)
-  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-  (yas-global-mode))
 
-(with-eval-after-load 'js
-  (define-key js-mode-map (kbd "M-.") nil))
-
-;;npm install -g eslint
-;;npm install -g typescript-language-server typescript
-;;https://emacs-lsp.github.io/lsp-mode/tutorials/reactjs-tutorial/
-
-
-(add-hook 'python-ts-mode-hook 'eglot-ensure)
-(setq python-indent-level 4)
-
-(use-package lsp-pyright
-  :ensure t
-  :hook (python-ts-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp))))  ; or lsp-deferred
-
-(add-hook 'js-ts-mode-hook 'eglot-ensure)
-(setq js-indent-level 2)
-
-
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-
-
-(unless (package-installed-p 'prettier-js)
-  (package-install 'prettier-js))
-
-
-(require 'prettier-js)
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'web-mode-hook 'prettier-js-mode)
-(add-hook 'js-ts-mode-hook 'prettier-js-mode)
-
-(use-package python-black
-  :demand t
-  :after python
-  :hook (python-ts-mode . python-black-on-save-mode))
